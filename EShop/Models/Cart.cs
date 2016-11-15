c666a417b58cc8039169fe016baa45d9b8edb39d
刘自东 <lzd931021@outlook.com>
2016-11-15 14:33:33 +0800

    修改了点外部引用，添加购物车实体

---
+++ EShop/Models/Cart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EShop.Models
+{
+    /// <summary>
+    /// 购物车实体
+    /// </summary>
+    public class Cart
+    {
+
+        [Key]
+        public string id { get; set; }
+
+        public string goods_id { get; set; }
+
+        public string user_id { get; set; }
+
+        public int number { get; set; }
+
+        public double price { get; set; }
+
+    }
+}
