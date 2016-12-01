7693d01d325c73f1de0dfe315ee682ee750c6ddf
刘自东 <lzd931021@outlook.com>
2016-12-01 20:06:22 +0800

    分离系统管理模块，目前只有目录，路由配置，具体事项由后期设计决定

--- EShop/App_Start/BundleConfig.cs
+++ EShop/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace EShop
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/echart").Include(
+                      "~/Scripts/ECharts/*.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
