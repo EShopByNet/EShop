0655aab5e3a3ee8cb63fd95c18a1999ad2fc41ac
刘自东 <lzd931021@outlook.com>
2016-12-03 09:46:39 +0800

    系统管理分离测试

---
+++ EShop/Areas/Admin/Controllers/CatController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EShop.Models;
+
+namespace EShop.Areas.Admin.Controllers
+{
+    public class CatController : Controller
+    {
+        private EShopDbContext db = new EShopDbContext();
+
+        // GET: Admin/Cat
+        public ActionResult Index()
+        {
+            return View(db.Cat.ToList());
+        }
+
+        // GET: Admin/Cat/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cat cat = db.Cat.Find(id);
+            if (cat == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cat);
+        }
+
+        // GET: Admin/Cat/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Cat/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,ParentId,Name,IsShow,IsDelete")] Cat cat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cat.Add(cat);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cat);
+        }
+
+        // GET: Admin/Cat/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cat cat = db.Cat.Find(id);
+            if (cat == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cat);
+        }
+
+        // POST: Admin/Cat/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,ParentId,Name,IsShow,IsDelete")] Cat cat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cat).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cat);
+        }
+
+        // GET: Admin/Cat/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cat cat = db.Cat.Find(id);
+            if (cat == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cat);
+        }
+
+        // POST: Admin/Cat/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cat cat = db.Cat.Find(id);
+            db.Cat.Remove(cat);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
