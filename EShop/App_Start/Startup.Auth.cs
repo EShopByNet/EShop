e9d78e6c374af091626d152543c4bc1a773a97b1
刘自东 <lzd931021@outlook.com>
2016-12-05 21:39:47 +0800

    layout 提交

--- EShop/App_Start/Startup.Auth.cs
+++ EShop/App_Start/Startup.Auth.cs
@@ -25,7 +25,7 @@ namespace EShop
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Account/Login"),
+                LoginPath = new PathString("/Account/Logindd"),
                 Provider = new CookieAuthenticationProvider
                 {
                     // Enables the application to validate the security stamp when the user logs in.
